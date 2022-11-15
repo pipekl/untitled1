598a393ca0b2f0e47a4064e801e407566833d5f9
Ivan <79262323423@yandex.ru>
2022-11-16 01:33:16 +0300

    test3

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
+        var str = "ggg";
     }
 
     private static void test() {
