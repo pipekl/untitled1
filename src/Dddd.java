f5d1429fed250ab96043da8ac36339bdeb7d73da
Ivan <79262323423@yandex.ru>
2022-12-16 16:53:52 +0300

    master13

--- src/Dddd.java
+++ src/Dddd.java
@@ -2,15 +2,12 @@ public class Dddd {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
+
     }
 
     private void test() {
         System.out.println(111);
         System.out.println(111);
-        System.out.println(111);
-        System.out.println(111);
-        System.out.println(111);
+
     }
 }
