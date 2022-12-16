5f2de6c1ea795885c349596edbddf5204c237035
Ivan <79262323423@yandex.ru>
2022-12-16 16:50:21 +0300

    master10

--- src/Dddd.java
+++ src/Dddd.java
@@ -2,5 +2,7 @@ public class Dddd {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
     }
 }
