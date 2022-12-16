d3928739bab2c3444b568a4eeecf5d492636de79
Ivan <79262323423@yandex.ru>
2022-12-16 16:55:34 +0300

    master114

--- src/Dddd.java
+++ src/Dddd.java
@@ -8,6 +8,8 @@ public class Dddd {
     private void test() {
         System.out.println(111);
         System.out.println(111);
+        System.out.println(111);
+        System.out.println(111);
 
     }
 }
