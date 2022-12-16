270cb5037ec98d03eb9936ffacdd0d2a602915bb
Ivan <79262323423@yandex.ru>
2022-12-16 16:32:51 +0300

    master1

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         var num = 1;
-        var num = 1;
-        var str = "ggg";
 
     }
 
