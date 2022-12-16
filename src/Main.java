3afbc5437cad38a68b39dc235697d907943423cb
Ivan <79262323423@yandex.ru>
2022-12-16 16:52:47 +0300

    my1

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,8 @@ public class Main {
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
 
     }
 }
