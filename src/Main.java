6aa9b570cafcd5ca0f4d607b895d5d49d6e22bb8
Ivan <79262323423@yandex.ru>
2022-12-27 13:44:20 +0300

    master34

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,6 @@ public class Main {
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello");
-        System.out.println("hello");
 
     }
 }
